6bda112c847664850a026e4941b42a8972d6ecbb
Arfin Parween <wbstartpracticing@gmail.com>
2024-07-05 21:00:14 +0530

    simplexmethod

--- src/simplexMethod.java
+++ src/simplexMethod.java
@@ -2,7 +2,7 @@ import java.util.Arrays;
 
 public class simplexMethod {
 
-    private final double[][] tableau; // The tableau
+    private final double[][] table; // The table
     private final int numberOfConstraints; // Number of constraints
     private final int numberOfOriginalVariables; // Number of original variables
 
@@ -10,25 +10,25 @@ public class simplexMethod {
         // Flag to indicate if the problem is a maximization problem
         numberOfConstraints = b.length;
         numberOfOriginalVariables = c.length;
-        tableau = new double[numberOfConstraints + 1][numberOfOriginalVariables + numberOfConstraints + 1];
+        table = new double[numberOfConstraints + 1][numberOfOriginalVariables + numberOfConstraints + 1];
 
-        // Initialize tableau
+        // Initialize table
         for (int i = 0; i < numberOfConstraints; i++) {
             for (int j = 0; j < numberOfOriginalVariables; j++) {
-                tableau[i][j] = A[i][j];
+                table[i][j] = A[i][j];
             }
         }
 
         for (int i = 0; i < numberOfConstraints; i++) {
-            tableau[i][numberOfOriginalVariables + i] = 1.0;
+            table[i][numberOfOriginalVariables + i] = 1.0;
         }
 
         for (int j = 0; j < numberOfOriginalVariables; j++) {
-            tableau[numberOfConstraints][j] = isMaximization ? c[j] : -c[j];
+            table[numberOfConstraints][j] = isMaximization ? c[j] : -c[j];
         }
 
         for (int i = 0; i < numberOfConstraints; i++) {
-            tableau[i][numberOfOriginalVariables + numberOfConstraints] = b[i];
+            table[i][numberOfOriginalVariables + numberOfConstraints] = b[i];
         }
     }
 
@@ -49,8 +49,8 @@ public class simplexMethod {
         int pivotColumn = -1;
         double max = 0;
         for (int j = 0; j < numberOfOriginalVariables + numberOfConstraints; j++) {
-            if (tableau[numberOfConstraints][j] > max) {
-                max = tableau[numberOfConstraints][j];
+            if (table[numberOfConstraints][j] > max) {
+                max = table[numberOfConstraints][j];
                 pivotColumn = j;
             }
         }
@@ -61,8 +61,8 @@ public class simplexMethod {
         int pivotRow = -1;
         double minRatio = Double.POSITIVE_INFINITY;
         for (int i = 0; i < numberOfConstraints; i++) {
-            if (tableau[i][pivotColumn] > 0) {
-                double ratio = tableau[i][numberOfOriginalVariables + numberOfConstraints] / tableau[i][pivotColumn];
+            if (table[i][pivotColumn] > 0) {
+                double ratio = table[i][numberOfOriginalVariables + numberOfConstraints] / table[i][pivotColumn];
                 if (ratio < minRatio) {
                     minRatio = ratio;
                     pivotRow = i;
@@ -73,17 +73,17 @@ public class simplexMethod {
     }
 
     private void pivot(int pivotRow, int pivotColumn) {
-        double pivotElement = tableau[pivotRow][pivotColumn];
+        double pivotElement = table[pivotRow][pivotColumn];
 
         for (int j = 0; j < numberOfOriginalVariables + numberOfConstraints + 1; j++) {
-            tableau[pivotRow][j] /= pivotElement;
+            table[pivotRow][j] /= pivotElement;
         }
 
         for (int i = 0; i < numberOfConstraints + 1; i++) {
             if (i != pivotRow) {
-                double ratio = tableau[i][pivotColumn];
+                double ratio = table[i][pivotColumn];
                 for (int j = 0; j < numberOfOriginalVariables + numberOfConstraints + 1; j++) {
-                    tableau[i][j] -= ratio * tableau[pivotRow][j];
+                    table[i][j] -= ratio * table[pivotRow][j];
                 }
             }
         }
@@ -95,8 +95,8 @@ public class simplexMethod {
             boolean isBasic = true;
             double value = 0;
             for (int j = 0; j < numberOfConstraints; j++) {
-                if (tableau[j][i] == 1) value = tableau[j][numberOfOriginalVariables + numberOfConstraints];
-                else if (tableau[j][i] != 0) isBasic = false;
+                if (table[j][i] == 1) value = table[j][numberOfOriginalVariables + numberOfConstraints];
+                else if (table[j][i] != 0) isBasic = false;
             }
             if (isBasic) solution[i] = value;
             else solution[i] = 0;
